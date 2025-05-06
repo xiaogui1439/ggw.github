fbcd6d09e2febfe86e69064eb01663bc289d34ea
xiaogui1439 <guiguangwei@gmail.com>
2025-05-06 20:49:41 +0800

    Update script.js

--- script.js
+++ script.js
@@ -37,11 +37,11 @@ noButton.addEventListener("click", function () {
     }
 
     // 图片变化（前 5 次变化）
-    if (clickCount === 1) mainImage.src = "images/shocked.png"; // 震惊
-    if (clickCount === 2) mainImage.src = "images/think.png";   // 思考
-    if (clickCount === 3) mainImage.src = "images/angry.png";   // 生气
-    if (clickCount === 4) mainImage.src = "images/crying.png";  // 哭
-    if (clickCount >= 5) mainImage.src = "images/crying.png";  // 之后一直是哭
+    if (clickCount === 1) mainImage.src = "./shocked.png"; // 震惊
+    if (clickCount === 2) mainImage.src = "./think.png";   // 思考
+    if (clickCount === 3) mainImage.src = "./angry.png";   // 生气
+    if (clickCount === 4) mainImage.src = "./crying.png";  // 哭
+    if (clickCount >= 5) mainImage.src = "./crying.png";  // 之后一直是哭
 
 });
 
